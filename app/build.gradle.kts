f38764ae5c6e113d052fd7f4c00bdc6ef09c4572
Muhammad Taha Nadeem <tahanadeem478@gmail.com>
2026-09-13 17:09:48 +0500

    Edited according to the code of ESP32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "2.1.0"
+    versionName = "2.1.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
